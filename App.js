3a96644082cde75d49da3232c27e45729456e64d
xenturion111 <rezaaditiyam@gmail.com>
2022-05-23 17:27:08 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      
+    </SafeAreaView>
+  );
+};
+
+
+
+export default App;
